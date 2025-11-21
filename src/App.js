0d2d84590a2c0600d63d2d6a93e9ff5e3c316f7a
iamsossem <iamsossem@gmail.com>
2025-11-21 11:35:50 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import './App.css';
+import Card from "./day2/Card";
+
 const App = ()=>{
   return (
     // 화면에 보여지는 영역 : JSX
-    <h1>리액트 시작</h1>
+    <div className="card-wrap">
+      <Card file='day1' title='day1' />
+      <Card file='day2' title='day2' />
+      <Card file='krewday' title='day3' />
+    </div>
   );
 }
 export default App;
