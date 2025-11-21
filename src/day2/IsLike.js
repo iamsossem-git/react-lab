95d8c38476811ccb1ea16f877628c9ddf045984c
iamsossem <iamsossem@gmail.com>
2025-11-21 17:40:10 +0900

    112117

--- src/day2/IsLike.js
+++ src/day2/IsLike.js
@@ -1,7 +1,13 @@
+import { useState } from "react"
 
 const IsLike = () => {
+  const [isLike, setIsLike] = useState(false);
   return (
-    <div>IsLike</div>
+    <div className="is-like">
+      <p>현재 좋아요 : {isLike?"ON":"OFF"}</p>
+      {isLike && <p>💕</p>}
+      <button onClick={()=>{setIsLike(!isLike)}}>좋아요 토글</button>
+    </div>
   )}
 
 export default IsLike
