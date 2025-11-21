a03ac93ffa819614962e32c4ef0a29721947e260
iamsossem <iamsossem@gmail.com>
2025-11-21 14:10:16 +0900

    update 112114

--- src/day2/Card.js
+++ src/day2/Card.js
@@ -1,14 +1,17 @@
 import './card.css';
+import number1Img from '../assets/img-01.png';
 // props : 부모->자식 : 객체로 전달
 const Card = ({file,title})=>{
   // console.log(data);
   // const fileName = data.file;
   // const {file} = data;
+  const PUBLIC = process.env.PUBLIC_URL;  
   return (
     <div className="card">
       <div className="img-wrap">
-        <img src={`/images/${file}.png`} alt="day1이미지" />
+        <img src={`${PUBLIC}/images/${file}.png`} alt="day1이미지" />
         <h2>{title}</h2>
+        {/* <img src={number1Img} alt='1번이미지'/> */}
       </div>
     </div>
   );
