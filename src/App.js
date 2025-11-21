e33232a0b294504739b17df1b897a0231964e587
iamsossem <iamsossem@gmail.com>
2025-11-21 10:30:34 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import Props from "./day1/Props";
 const App = ()=>{
   return (
-    <div className="app">
-      <h1>리액트 실습</h1>
-      <Props user='춘향이' text='첫번째' idx={1} />
-      <Props user='홍길동' text='두번째' idx={2} />
-      <Props user='갑순이' text='세번째' idx={3} />
-    </div>
+    // 화면에 보여지는 영역 : JSX
+    <h1>리액트 시작</h1>
   );
 }
 export default App;
